0f9556a6af800100b9746d209d2791691e084b32
Okpe onoja <okpeonoja18@gmail.com>
2022-12-29 00:24:35 +0100

    completed

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,5 +1,4 @@
-const { Configuration, OpenAIApi } = require('openai');
-
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const path = require("path")
 const express = require("express")
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv')
+dotenv.config({path: "./config.env"})
 
 const app = express()
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false}))
+
 app.use(express.static(path.join(__dirname, 'public')))
+
 app.use("/openai", require("./routes/openaiRoute"))
 
 const PORT = process.env.PORT || 3000
